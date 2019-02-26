87c9f9d2a6caff5980cf9b5ed800b8935a701a14
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-02-26 20:42:59 +0700

    clean up unused import

--- app/src/main/java/com/dicoding/moviestate/movie/MovieTopRatedFragment.java
+++ app/src/main/java/com/dicoding/moviestate/movie/MovieTopRatedFragment.java
@@ -1,6 +1,5 @@
 package com.dicoding.moviestate.movie;
 
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dicoding.moviestate.R;
