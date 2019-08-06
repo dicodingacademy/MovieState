92ce626395081951f31de9a725147c814e1faf37
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-08-06 10:54:04 +0700

    delete annotation and change to abstract

--- app/src/main/java/com/dicoding/moviestate/base/BaseAppCompatActivity.java
+++ app/src/main/java/com/dicoding/moviestate/base/BaseAppCompatActivity.java
@@ -1,14 +1,12 @@
 package com.dicoding.moviestate.base;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.androidnetworking.AndroidNetworking;
 
-@SuppressLint("Registered")
-public class BaseAppCompatActivity extends AppCompatActivity {
+public abstract class BaseAppCompatActivity extends AppCompatActivity {
 
     public static final String KEY_TITLE = "title";
     public static final String KEY_FRAGMENT = "fragment";
