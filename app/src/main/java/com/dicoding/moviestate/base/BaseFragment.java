89594dac4a7912d148db60e254a53d41bcaec284
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-08-09 17:05:48 +0700

    make it generic and add some abstract method

--- app/src/main/java/com/dicoding/moviestate/base/BaseFragment.java
+++ app/src/main/java/com/dicoding/moviestate/base/BaseFragment.java
@@ -1,14 +1,30 @@
 package com.dicoding.moviestate.base;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import com.dicoding.moviestate.network.MovieDataSources;
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment<T extends ViewModel> extends Fragment {
 
     public static final String KEY_MOVIES = "movies";
 
+    public T movieViewModel;
+
+    public abstract Class<T> provideViewModelClass();
+
     public MovieDataSources getMovieDataSources() {
         return new MovieDataSources();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        movieViewModel = ViewModelProviders.of(this).get(provideViewModelClass());
     }
 }
